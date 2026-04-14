b5be821ec6a6a476658e1cb71fd6b0e119122eed
Seif Mattar <seifmamdouh@yahoo.com>
2026-04-14 03:32:36 +0200

    issue solve

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://backend-production-5ee0.up.railway.app";
 
 const api = axios.create({
   baseURL: API_URL,
